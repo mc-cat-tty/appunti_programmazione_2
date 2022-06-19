e655e8fdad50838b5e3196201d7f61a408a22e46
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2022-06-19 16:57:54 +0200

    done simulazione agenda

--- simulazioni/agenda/liste.h
+++ simulazioni/agenda/liste.h
@@ -1,14 +1,13 @@
-/*******************************/
-/* HEADER MODULO "liste" */
-/*******************************/
+#pragma once
 
 #include "tipo.h"
-struct elem
-{
+
+
+struct elem {
        tipo_inf inf;
        elem* pun ;
        elem* prev;
-} ;
+};
 
 typedef elem* lista ;
 	
@@ -20,3 +19,10 @@ lista delete_elem(lista, elem*);
 tipo_inf head(lista);
 elem* new_elem(tipo_inf);
 elem* search(lista, tipo_inf);
+
+/**
+ * @brief insert element in the list while keeping it sorted
+ * 
+ * @return lista freshly updated list
+ */
+lista insert_elem_sorted(lista, elem*);
--- simulazioni/agenda/tipo.h
+++ simulazioni/agenda/tipo.h
@@ -1,17 +1,35 @@
-/*******************************/
-/* HEADER MODULO "tipo" */
-/*******************************/
+#pragma once
 
 
+const int DATA_MAX_LEN = 7;
+const int ORA_I_MAX_LEN = 5;
+const int ORA_F_MAX_LEN = 5;
+const int DESCR_MAX_LEN = 51;
 
-struct tipo_inf{
-	char data[7];
-	char ora_i[5];
-	char ora_f[5];
-	char descr[51];
+struct appuntamento {
+	char data[DATA_MAX_LEN];
+	char ora_i[ORA_I_MAX_LEN];
+	char ora_f[ORA_F_MAX_LEN];
+	char descr[DESCR_MAX_LEN];
 };
 
+typedef appuntamento tipo_inf;
 
-int compare(tipo_inf,tipo_inf);
-void copy(tipo_inf&,tipo_inf);
-void print(tipo_inf);
+/**
+ * @brief compare two appointments
+ * 
+ * @return int zero x and t have the same date and staring hour; x.ora_i - y.ora_i (lexicographically) otherwise
+ */
+int compare(tipo_inf x, tipo_inf y);
+
+/**
+ * @brief copy the entire 'appuntamento' structure from src to dst
+ * 
+ */
+void copy(tipo_inf &src, tipo_inf dst);
+
+/**
+ * @brief print appointment x
+ * 
+ */
+void print(tipo_inf x);
